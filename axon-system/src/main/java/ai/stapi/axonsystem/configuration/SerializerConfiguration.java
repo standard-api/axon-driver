d32fafe655cf1902e841af51c7b255aa9d9135fd
clickout <mick.voros@gmail.com>
2023-06-08 21:26:51 +0200

    Axon System - Configuration improvements

--- axon-system/src/main/java/ai/stapi/axonsystem/configuration/SerializerConfiguration.java
+++ axon-system/src/main/java/ai/stapi/axonsystem/configuration/SerializerConfiguration.java
@@ -1,22 +1,23 @@
 package ai.stapi.axonsystem.configuration;
 
+import ai.stapi.configuration.SerializationConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
+@AutoConfigureAfter(SerializationConfiguration.class)
 public class SerializerConfiguration {
-
+  
   @Bean
   @Qualifier("messageSerializer")
   @ConditionalOnBean(ObjectMapper.class)
-  public Serializer messageSerializer(
-      ObjectMapper objectMapper
-  ) {
+  public Serializer customMessageSerializer(ObjectMapper objectMapper) {
     return JacksonSerializer
         .builder()
         .objectMapper(objectMapper)
