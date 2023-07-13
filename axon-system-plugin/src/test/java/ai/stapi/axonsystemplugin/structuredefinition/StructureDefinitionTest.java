881258133c0e36bcb06f2058da22ebdbc0ebea21
clickout <mick.voros@gmail.com>
2023-07-13 18:32:42 +0200

    [axon-system-plugin] - StructureDefinition - refactor ImportStructureDefinition handler to support all fields dynamically

--- axon-system-plugin/src/test/java/ai/stapi/axonsystemplugin/structuredefinition/StructureDefinitionTest.java
+++ axon-system-plugin/src/test/java/ai/stapi/axonsystemplugin/structuredefinition/StructureDefinitionTest.java
@@ -2,13 +2,14 @@ package ai.stapi.axonsystemplugin.structuredefinition;
 
 import ai.stapi.graphsystem.structuredefinition.command.importStructureDefinitionFromSource.ImportStructureDefinition;
 import ai.stapi.graphsystem.structuredefinition.command.importStructureDefinitionFromSource.StructureDefinitionImported;
-import ai.stapi.schema.structuredefinition.RawStructureDefinitionData;
 import ai.stapi.schema.structuredefinition.StructureDefinitionId;
 import ai.stapi.test.domain.DomainTestCase;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +24,12 @@ class StructureDefinitionTest extends DomainTestCase {
     var fixtureFilePath = this.getFixtureFilePath("element.profile.canonical.json");
     var file = new File(fixtureFilePath);
     var fileContent = FileUtils.readFileToString(file, "UTF-8");
-    RawStructureDefinitionData structureDefinitionSource;
+    Map<String, Object> structureDefinitionSource;
     try {
       structureDefinitionSource = this.objectMapper.readValue(
-          fileContent, 
-          RawStructureDefinitionData.class
+          fileContent,
+          new TypeReference<>() {
+          }
       );
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
