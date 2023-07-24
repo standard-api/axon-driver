f13a23dcb671294826263838dd43fe7238de610d
clickout <mick.voros@gmail.com>
2023-07-24 14:40:20 +0200

    [axon-system-plugin] Add test for CommitCommandFixturesLineRunner

--- axon-system-plugin/src/main/java/ai/stapi/axonsystemplugin/commandpersisting/CommitCommandFixturesLineRunner.java
+++ axon-system-plugin/src/main/java/ai/stapi/axonsystemplugin/commandpersisting/CommitCommandFixturesLineRunner.java
@@ -21,7 +21,14 @@ public class CommitCommandFixturesLineRunner implements CommandLineRunner {
   }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
+    var outputDirectoryPath = getOutputDirectoryPath(args);
+    this.commandGateway.sendAndWait(new CommitCommandFixtures(outputDirectoryPath));
+    var exitCode = SpringApplication.exit(this.applicationContext, () -> 0);
+    System.exit(exitCode);
+  }
+
+  public static String getOutputDirectoryPath(String... args) {
     if (args.length == 0) {
       throw new RuntimeException("Please specify path where to commit persisted commands as command line argument.");
     }
@@ -29,9 +36,7 @@ public class CommitCommandFixturesLineRunner implements CommandLineRunner {
         .filter(arg -> arg.startsWith("_outputPath:"))
         .map(arg -> arg.replace("_outputPath:", ""))
         .findFirst();
-    
-    this.commandGateway.sendAndWait(new CommitCommandFixtures(path.orElse(args[0])));
-    var exitCode = SpringApplication.exit(this.applicationContext, () -> 0);
-    System.exit(exitCode);
+
+    return path.orElse(args[0]);
   }
 }
