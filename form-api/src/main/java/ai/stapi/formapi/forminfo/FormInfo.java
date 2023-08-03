41f2286a219ab705549ba9109153f80b1d34c743
clickout <mick.voros@gmail.com>
2023-08-03 22:08:26 +0200

    [form-api] Introduce FromInfoMapper

---
+++ form-api/src/main/java/ai/stapi/formapi/forminfo/FormInfo.java
@@ -0,0 +1,34 @@
+package ai.stapi.formapi.forminfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class FormInfo {
+    
+    private final Map<String, Set<String>> possibleTargets;
+    private final String resourceType;
+    private final Boolean requiresResourceId;
+
+    public FormInfo(
+        Map<String, Set<String>> possibleTargets, 
+        String resourceType, 
+        Boolean requiresResourceId
+    ) {
+        this.possibleTargets = possibleTargets;
+        this.resourceType = resourceType;
+        this.requiresResourceId = requiresResourceId;
+    }
+
+    public Map<String, Set<String>> getPossibleTargets() {
+        return possibleTargets;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public Boolean getRequiresResourceId() {
+        return requiresResourceId;
+    }
+}
